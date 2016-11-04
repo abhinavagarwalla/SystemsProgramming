7dab520643d3a41df59297ce0f2cb8497529fb94
Abhinav Agarwalla <navabhi174@gmail.com>
2016-11-04 15:32:08 +0530

    Support for self-modifying programs

--- assmu.c
+++ assmu.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<iostream>
+using namespace std;
 
 int f(char t[]){
 	if (strcmp(t, "al")==0) return 0;
@@ -26,6 +28,38 @@ int f(char t[]){
 	return 200;
 }
 
+int cmd2int(char t[], char cmd[]){
+	int base = 0;
+	if(strcmp(cmd,"mov")==0)base = 176;
+ 
+	if (strcmp(t, "al")==0) return base+0;
+        else if (strcmp(t, "cl")==0) return base+1;
+        else if (strcmp(t, "dl")==0) return base+2;
+        else if (strcmp(t, "bl")==0) return base+3;
+
+        else if (strcmp(t, "ah")==0) return base+4;
+        else if (strcmp(t, "ch")==0) return base+5;
+        else if (strcmp(t, "dh")==0) return base+6;
+        else if (strcmp(t, "bh")==0) return base+7;
+	return 200;
+}
+
+int getindex(char t[]){
+	int k = int(t[4])-48;
+	//cout << "for  " << t[4] << ", value of index returned: " << k << endl;
+	return k;
+}
+
+bool change_seg(char t[]){
+	if(int(t[0])>47 & int(t[0])<58)return false;
+	//cout << "Value of t in change_seg: " << t << endl;
+	int k1=0, k2=0;
+	if(t[2]==':')k1=1;
+	if(t[3]=='[')k2=1;
+	//{cout << "Value of k1, k2: " << k1 << "," << k2 << endl;return true;}
+	return k1*k2;
+}
+	
 int isword(char t[]){
 	if (t[1]=='x' || t[1]=='p' || t[1]=='i' ) return 1;
 	return 0;
@@ -40,8 +74,9 @@ int main(){
 	char t[10], u[10], v[10], sy[200];
 	int i=0;
 	int jlines[100], jlabel[100];
+	int clines[100], clabel[100];
 	int ins[100];
-	int k=0;
+	int k=0, ck=0;
 	
 	do {
 		scanf("%s", t);
@@ -90,6 +125,14 @@ int main(){
 			}
 			else {
 				ins[i-2] = 176+f(u)+8*isword(u);
+				if (change_seg(v)){
+					ins[i-2] = cmd2int(u, t);
+					ins[i-1] = -1;
+					clines[ck] = i-1; 
+					clabel[ck] = ins[getindex(v)];
+					//cout << "value at clabel" << clabel[ck];
+					ck++;
+				}
 				if (isword(u)){
 					i++;
 					ins[i-2] = atoi(v)%256;
@@ -109,6 +152,9 @@ int main(){
 	for(int j=0;j<k;j++){
 		ins[jlines[j]] = sy[jlabel[j]]-jlines[j]-1;
 	}
+	for(int j=0;j<ck;j++){
+                ins[clines[j]] = clabel[j];
+        }
 	for(int j=0;j<i;j++){
 		printf("%d\n", ins[j]);
 	}
